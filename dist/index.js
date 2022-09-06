434c0b47ddd66ef60c11f046d0d497f5f8b5a9cd
jstaresincic <jurica.staresincic@minus5.hr>
2022-09-06 15:50:59 +0200

    log incoming settings

--- dist/index.js
+++ dist/index.js
@@ -165,18 +165,22 @@ const deleteVersions = __webpack_require__(887);
 async function run() {
   try {
     const inputs = {
-      owner: getInput('owner') || context.repo.owner,
-      repo: getInput('repo') || context.repo.repo,
-      keepCnt: getInput('keepCnt'),
-      dryRun: getInput('dryRun'),
-      token: getInput('token'),
-    }
+      owner: getInput("owner") || context.repo.owner,
+      repo: getInput("repo") || context.repo.repo,
+      keepCnt: getInput("keepCnt"),
+      dryRun: getInput("dryRun"),
+      token: getInput("token"),
+    };
+    info("Input values:");
+    Object.values(inputs).map((v) => info(v));
     const deleted = await deleteVersions(inputs);
-	if (inputs.dryRun) {
-		info('Dry run is NOT deleting packages, list below is what would be deleted')
-	}
-    deleted.forEach(package => {
-      package.versions.forEach(v => {
+    if (inputs.dryRun) {
+      info(
+        "Dry run is NOT deleting packages, list below is what would be deleted"
+      );
+    }
+    deleted.forEach((package) => {
+      package.versions.forEach((v) => {
         info(`Deleted ${package.name} version ${v.version}`);
       });
     });
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const { getInput, setFailed, info } = require('@actions/core');
-const { context } = require('@actions/github');
-const deleteVersions = require('./delete-versions');
+const { getInput, setFailed, info } = require("@actions/core");
+const { context } = require("@actions/github");
+const deleteVersions = require("./delete-versions");
 
 async function run() {
   try {
     const inputs = {
-      owner: getInput('owner') || context.repo.owner,
-      repo: getInput('repo') || context.repo.repo,
-      keepCnt: getInput('keepCnt'),
-      dryRun: getInput('dryRun'),
-      token: getInput('token'),
-    }
+      owner: getInput("owner") || context.repo.owner,
+      repo: getInput("repo") || context.repo.repo,
+      keepCnt: getInput("keepCnt"),
+      dryRun: getInput("dryRun"),
+      token: getInput("token"),
+    };
+    info("Input values:");
+    Object.values(inputs).map((v) => info(v));
     const deleted = await deleteVersions(inputs);
-	if (inputs.dryRun) {
-		info('Dry run is NOT deleting packages, list below is what would be deleted')
-	}
-    deleted.forEach(package => {
-      package.versions.forEach(v => {
+    if (inputs.dryRun) {
+      info(
+        "Dry run is NOT deleting packages, list below is what would be deleted"
+      );
+    }
+    deleted.forEach((package) => {
+      package.versions.forEach((v) => {
         info(`Deleted ${package.name} version ${v.version}`);
       });
     });
